fdb9df799796d2f2ddd4aa8f9595d989135caa1c
Lucus0203 <liting68@163.com>
2014-08-28 00:44:03 +0800

    Initial Commit

---
+++ Three_Vs_Three/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Three_Vs_Three
+//
+//  Created by lucus on 14-8-28.
+//  Copyright (c) 2014年 lucus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
